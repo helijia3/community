47bf9bbb7d4b04fd9d0103aaaa019d503193600b
helj3 <45003422+helj3@users.noreply.github.com>
2020-07-24 18:38:37 +0800

    second commit

--- src/main/java/hlj/majiang/community/controller/IndexController.java
+++ src/main/java/hlj/majiang/community/controller/IndexController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author helj
@@ -12,11 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date 2020-07-24 上午 9:18
  */
 @Controller
-public class HelloController {
-    @RequestMapping("/hello")
+public class IndexController {
+    @RequestMapping("/")
     //@ResponseBody
-    public String hello(@RequestParam(name = "name") String name, Model model){
-         model.addAttribute("name", name);
-        return "hello";
+    public String index(){
+        return "index";
     }
 }
